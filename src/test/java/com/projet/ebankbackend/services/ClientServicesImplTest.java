35db76b943302de263e234eb521fcd6ddb72d9ac
nobasidi <nobasidi@gmail.com>
2022-09-16 19:38:51 +0100

    ceci concerne les testes pour tester

--- src/test/java/com/projet/ebankbackend/services/ClientServicesImplTest.java
+++ src/test/java/com/projet/ebankbackend/services/ClientServicesImplTest.java
@@ -1,12 +1,16 @@
 package com.projet.ebankbackend.services;
 
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
 import org.junit.jupiter.api.Test;
 
 public class ClientServicesImplTest {
     @Test
     void testGetAccount() 
     {
-        //soon
+        String totest="sidiba";
+        String totest1="omarba";
+        assertNotEquals(totest, totest1);
     }
 
     @Test
